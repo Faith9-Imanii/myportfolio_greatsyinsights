e6772c58823c69e6a3cdd290b4f77809fc374798
Faith9-Imanii <faimanishiku@gmail.com>
2025-10-03 15:19:02 +0300

    Fix middleware configuration

--- myportfolio/settings.py
+++ myportfolio/settings.py
@@ -1,9 +1,10 @@
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
-    "whitenoise.middleware.WhiteNoiseMiddleware",  # add this
+    "whitenoise.middleware.WhiteNoiseMiddleware",   # for static files on Render
     "django.contrib.sessions.middleware.SessionMiddleware",
-    ...
+    "django.middleware.common.CommonMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
-
-# Add this at the bottom of settings.py
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
